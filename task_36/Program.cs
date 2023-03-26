74dc0d11696f91e02e054690d3d69cc839247d50
Ekaterina <peka29@yandex.ru>
2023-03-26 20:10:41 +0300

    Решила задачу 36

--- task_36/Program.cs
+++ task_36/Program.cs
@@ -4,3 +4,30 @@
 // [-4, -6, 89, 6] -> 0
 
 
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-10, 10); // [-10, 10]
+}
+
+void ReleaseArray(int[] array)
+{
+    int sum_n = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (i % 2 > 0)
+           sum_n += array[i];
+    Console.WriteLine($"сумму элементов,стоящих на нечётных позиция = {sum_n}");
+    }
+
+void PrintArray(int[] array)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+Console.Clear();
+Console.Write($"n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+PrintArray(array);
+ReleaseArray(array);
